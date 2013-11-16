53204a04ded59c69619cb0fd051a4fbbb23d85c8
nawshad <me.naws@gmail.com>
2013-11-16 01:41:57 -0500

    Update des.cpp

--- des.cpp
+++ des.cpp
@@ -202,7 +202,7 @@ string hex_conversion(string de_msg,string conv_table[23],string hex[23]){
 string des_process(string message,string key,bool encrypt){
 	string conv_table[23]={
    		"0000",
-	    "0001",
+	    	"0001",
    		"0010",
    		"0011",
    		"0100",
@@ -218,7 +218,7 @@ string des_process(string message,string key,bool encrypt){
    		"pad",
    		"pad",
    		"pad",
-	    "1010",
+	    	"1010",
    		"1011",
    		"1100",
    		"1101",
@@ -293,81 +293,80 @@ string des_process(string message,string key,bool encrypt){
    };
    
    int sbox[8][4][16]={
-	    {
-			14, 4, 13, 1, 2, 15, 11, 8, 3,  10, 6,  12, 5, 9,  0, 7,
-		     0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12,  11, 9, 5,  3, 8,
-			 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9,   7, 3, 10, 5, 0,
-			15, 12, 8, 2,  4, 9, 1,  7, 5,  11, 3,  14, 10, 0, 6, 13
+   	{
+		14, 4, 13, 1, 2, 15, 11, 8, 3,  10, 6,  12, 5, 9,  0, 7,
+	     	0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12,  11, 9, 5,  3, 8,
+		 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9,   7, 3, 10, 5, 0,
+		15, 12, 8, 2,  4, 9, 1,  7, 5,  11, 3,  14, 10, 0, 6, 13
     	},
-		{
-			15, 1, 8, 14, 6, 11, 3,  4,  9,  7, 2, 13, 12,  0, 5, 10,
-            3, 13, 4, 7,  15, 2, 8, 14, 12,  0, 1, 10,  6,  9, 11, 5,
-            0, 14, 7, 11, 10, 4, 13, 1,  5,  8, 12, 6,  9,  3,  2, 15,
-            13, 8, 10, 1, 3, 15, 4,  2, 11,  6, 7, 12,  0,  5, 14,  9
-		},
-		{
-			10, 0, 9, 14, 6, 3, 15, 5,  1, 13, 12, 7, 11, 4,  2,  8,
-			13, 7, 0, 9,  3, 4,  6, 10, 2, 8,  5, 14, 12, 11, 15, 1,
-			13, 6, 4, 9,  8, 15, 3, 0, 11, 1,  2, 12, 5,  10, 14, 7,
-			1, 10, 13, 0, 6, 9,  8, 7,  4, 15, 14, 3, 11, 5,  2, 12
-		},
-		{
-			7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
-		    13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
-			10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
-			3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14,
-		},
-		{
-			2,  12, 4, 1,  7, 10, 11, 6, 8, 5, 3,  15, 13, 0, 14, 9,
-			14, 11, 2, 12, 4, 7,  13, 1, 5, 0, 15, 10, 3,  9, 8,  6,
-			4,  2,  1, 11, 10,13, 7,  8, 15,9, 12, 5,  6,  3, 0,  14,
-			11, 8, 12, 7,  1, 14, 2, 13, 6, 15, 0, 9,  10, 4, 5,  3
-		},
-		{
-		    12, 1, 10, 15,9, 2, 6, 8, 0, 13, 3, 4, 14, 7,  5, 11,
-	        10, 15, 4, 2, 7, 12,9, 5, 6, 1, 13, 14, 0, 11, 3,  8,
-			9,  14,15, 5, 2, 8, 12,3, 7, 0, 4,  10, 1, 13, 11, 6,
-			4,  3,  2, 12,9, 5, 15,10,11,14,1,  7,  6, 0,  8, 13
-		},
-		{
-		    4, 11, 2,  14, 15, 0, 8, 13, 3,  12, 9, 7,  5, 10, 6, 1,
-	       13, 0,  11, 7,  4,  9, 1, 10, 14,  3, 5, 12, 2, 15, 8, 6,
-			1, 4,  11, 13, 12, 3, 7, 14, 10, 15, 6,  8, 0,  5, 9, 2,
-			6, 11, 13, 8,  1,  4, 10, 7, 9,  5,  0, 15, 14, 2, 3, 12
-		},
-		{
-			 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3,  14, 5, 0, 12, 7,
-			 1, 15,13, 8, 10, 3, 7,  4, 12, 5, 6,  11, 0, 14, 9, 2,
-		     7, 11, 4, 1, 9, 12, 14, 2,  0, 6, 10, 13, 15, 3, 5, 8,
-		     2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9,  0, 3,  5, 6, 11
-		}
-   	};
+	{
+		15, 1, 8, 14, 6, 11, 3,  4,  9,  7, 2, 13, 12,  0, 5, 10,
+            	3, 13, 4, 7,  15, 2, 8, 14, 12,  0, 1, 10,  6,  9, 11, 5,
+            	0, 14, 7, 11, 10, 4, 13, 1,  5,  8, 12, 6,  9,  3,  2, 15,
+            	13, 8, 10, 1, 3, 15, 4,  2, 11,  6, 7, 12,  0,  5, 14,  9
+	},
+	{
+		10, 0, 9, 14, 6, 3, 15, 5,  1, 13, 12, 7, 11, 4,  2,  8,
+		13, 7, 0, 9,  3, 4,  6, 10, 2, 8,  5, 14, 12, 11, 15, 1,
+		13, 6, 4, 9,  8, 15, 3, 0, 11, 1,  2, 12, 5,  10, 14, 7,
+		1, 10, 13, 0, 6, 9,  8, 7,  4, 15, 14, 3, 11, 5,  2, 12
+	},
+	{
+		7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+	    	13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+		10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+		3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14,
+	},
+	{
+		2,  12, 4, 1,  7, 10, 11, 6, 8, 5, 3,  15, 13, 0, 14, 9,
+		14, 11, 2, 12, 4, 7,  13, 1, 5, 0, 15, 10, 3,  9, 8,  6,
+		4,  2,  1, 11, 10,13, 7,  8, 15,9, 12, 5,  6,  3, 0,  14,
+		11, 8, 12, 7,  1, 14, 2, 13, 6, 15, 0, 9,  10, 4, 5,  3
+	},
+	{
+	    	12, 1, 10, 15,9, 2, 6, 8, 0, 13, 3, 4, 14, 7,  5, 11,
+		10, 15, 4, 2, 7, 12,9, 5, 6, 1, 13, 14, 0, 11, 3,  8,
+		9,  14,15, 5, 2, 8, 12,3, 7, 0, 4,  10, 1, 13, 11, 6,
+		4,  3,  2, 12,9, 5, 15,10,11,14,1,  7,  6, 0,  8, 13
+	},
+	{
+	    	4, 11, 2,  14, 15, 0, 8, 13, 3,  12, 9, 7,  5, 10, 6, 1,
+	       	13, 0,  11, 7,  4,  9, 1, 10, 14,  3, 5, 12, 2, 15, 8, 6,
+		1, 4,  11, 13, 12, 3, 7, 14, 10, 15, 6,  8, 0,  5, 9, 2,
+		6, 11, 13, 8,  1,  4, 10, 7, 9,  5,  0, 15, 14, 2, 3, 12
+	},
+	{
+	 	13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3,  14, 5, 0, 12, 7,
+		 1, 15,13, 8, 10, 3, 7,  4, 12, 5, 6,  11, 0, 14, 9, 2,
+	     	7, 11, 4, 1, 9, 12, 14, 2,  0, 6, 10, 13, 15, 3, 5, 8,
+	     	2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9,  0, 3,  5, 6, 11
+	}
+   };
   
-  	int pc1[8][7]={
-        57, 49, 41, 33, 25, 17, 9,
-        1,  58, 50, 42, 34, 26, 18,
-	    10, 2,  59, 51, 43, 35, 27,
-	    19, 11, 3,  60, 52, 44, 36,
-	    63, 55, 47, 39, 31, 23, 15,
-	    7,  62, 54, 46, 38, 30, 22,
-	    14, 6,  61, 53, 45, 37, 29,
-	    21, 13, 5,  28, 20, 12, 4
-  	};
+  int pc1[8][7]={
+	57, 49, 41, 33, 25, 17, 9,
+	1,  58, 50, 42, 34, 26, 18,
+	 10, 2,  59, 51, 43, 35, 27,
+    	19, 11, 3,  60, 52, 44, 36,
+    	63, 55, 47, 39, 31, 23, 15,
+    	7,  62, 54, 46, 38, 30, 22,
+    	14, 6,  61, 53, 45, 37, 29,
+    	21, 13, 5,  28, 20, 12, 4
+  };
  
-  	int pc2[6][8]={
-		14, 17, 11, 24, 1,  5,  3, 28,
-		15, 6,  21, 10, 23, 19, 12, 4,
-		26, 8,  16, 7,  27, 20, 13, 2,
-		41, 52, 31, 37, 47, 55, 30, 40,
-		51, 45, 33, 48, 44, 49, 39, 56,
-		34, 53, 46, 42, 50, 36, 29, 32
-  	};
- 
-  	int rotating_schedule[16]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+  int pc2[6][8]={
+	14, 17, 11, 24, 1,  5,  3, 28,
+	15, 6,  21, 10, 23, 19, 12, 4,
+	26, 8,  16, 7,  27, 20, 13, 2,
+	41, 52, 31, 37, 47, 55, 30, 40,
+	51, 45, 33, 48, 44, 49, 39, 56,
+	34, 53, 46, 42, 50, 36, 29, 32
+  };
+
+  int rotating_schedule[16]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
     /*necessary tables initialization ends*/
-   	/*hexadecimal msg and key input and conversion to binary starts*/
-	 
-  	string bin_key,input; 
+    /*hexadecimal msg and key input and conversion to binary starts*/
+	string bin_key,input; 
   	cout<<"Input hex message:";
   	for(int i=0;i<16;i++){
  	 	cout<<message[i];
@@ -410,36 +409,35 @@ string des_process(string message,string key,bool encrypt){
 	int round=16;
 	string key_holder[round],fe_key;
 	for(int i=0;i<round;i++){
-		   int iteration=i;
-		   string C1=rol_keys(Co,rotating_schedule,iteration%round);
-		   string D1=rol_keys(Do,rotating_schedule,iteration%round);   
-		   
-		   fe_key=fourty_eight_bit_key(C1,D1,pc2);
-		   key_holder[i]=fe_key;
+	   int iteration=i;
+	   string C1=rol_keys(Co,rotating_schedule,iteration%round);
+	   string D1=rol_keys(Do,rotating_schedule,iteration%round);   
+	   
+	   fe_key=fourty_eight_bit_key(C1,D1,pc2);
+	   key_holder[i]=fe_key;
 		      		
-	       Co=C1;
-	       Do=D1;
-		   /*key processing ends*/
+       	   Co=C1;
+	   Do=D1;		   /*key processing ends*/
 	}
 	
 	int i=0;      
 	while(round>0){
 	    cout<<"\nRound:"<<i+1<<endl;
-		Sleep(500);
+	    Sleep(500);
 		
-		string ep_msg=expanded_msg(Ro,ep);
+	    string ep_msg=expanded_msg(Ro,ep);
 	    cout<<"Expanded msg(ER["<<i<<"]):\n"<<ep_msg<<endl;  
 		
 	    if(encrypt==false){
             fe_key="";
-		    for(int j=0;j<48;j++)fe_key=fe_key+key_holder[round-1][j];
-		    cout<<"\nkey["<<round<<"]:\n"<<fe_key<<endl;
+	    	for(int j=0;j<48;j++)fe_key=fe_key+key_holder[round-1][j];
+	    	cout<<"\nkey["<<round<<"]:\n"<<fe_key<<endl;
   	    }
   	    else{
   	    	fe_key="";
-		    for(int j=0;j<48;j++)fe_key=fe_key+key_holder[i][j];
+		for(int j=0;j<48;j++)fe_key=fe_key+key_holder[i][j];
 		    cout<<"\nkey["<<i+1<<"]:\n"<<fe_key<<endl;
-        }
+            }
   	    
 	    string xored_message=xored_msg(ep_msg,fe_key);
 	    cout<<"Xored Message(E(R["<<i<<"]+K["<<i+1<<"]):\n"<<xored_message<<endl;
@@ -450,16 +448,16 @@ string des_process(string message,string key,bool encrypt){
 	    string permuted_msg=permuted_message(substituted_msg,pf);
 	    cout<<"Permuted message(f(R["<<i<<"],K["<<i+1<<"])):\n"<<permuted_msg<<endl;
 	    
-		string L1=Ro;
-		cout<<"L["<<i+1<<"]:"<<L1<<endl;
+	    string L1=Ro;
+	    cout<<"L["<<i+1<<"]:"<<L1<<endl;
 		
-		string R1=xored_msg(Lo,permuted_msg);
+	    string R1=xored_msg(Lo,permuted_msg);
 	    cout<<"R["<<i+1<<"]:"<<R1<<endl;
 	    
 	    Lo=L1;
-		Ro=R1;
+	    Ro=R1;
 
-		round--;
+	    round--;
 	    i++;	    	    
 	}
 	
@@ -481,7 +479,7 @@ bool input_verifier(string message){
 	 
 	 for(int i=0;i<message.length();i++){
 	 	for(int j=0;j<hex_standard.length();j++){
-  	    	if(message[i]==hex_standard[j])count++;	
+  	    		if(message[i]==hex_standard[j])count++;	
   	    } 	
 	 }
 	 
@@ -525,7 +523,7 @@ int main(){
 			Sleep(1000);
 			string message="0123456789ABCDEF",key="133457799BBCDFF1";	
 			string encrypted_msg=des_process(message,key,true);
-		    cout<<"Finally!!Hex Encrypted Message:"<<encrypted_msg<<endl;
+		    	cout<<"Finally!!Hex Encrypted Message:"<<encrypted_msg<<endl;
 	
 			char ch;
 			cout<<"Want to see decryption?(y/n):";
@@ -537,7 +535,7 @@ int main(){
 			}
 		  }
 	 	  else{
-		     cout<<"Encrypt/Decrypt(e/d)?:";
+			 cout<<"Encrypt/Decrypt(e/d)?:";
 		 	 cin>>endc;
 		 	 if(tolower(endc)=='e')encrypt=true;
 		 	 else encrypt=false;
